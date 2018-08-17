6c91addfb810cff324ca5dd64798508beaf3b3d7
Rob <simonyan.dev@gmail.com>
2018-08-17 15:10:00 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Main from './Main'
+
+
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <Main/>
+                <Main/>
+                <Main/>
+            </div>
+        );
+    }
+}
+export default App;
