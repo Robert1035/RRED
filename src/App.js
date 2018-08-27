6b618cb0c304f1287d4a752eb174713be5871bae
Rob <simonyan.dev@gmail.com>
2018-08-27 16:21:53 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import Main from './Main'
+import '../node_modules/bulma/css/bulma.css'
+import reducer from './reducer'
+import task from './task'
 
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
 
     render() {
         return (
-            <div>
-                <Main/>
-                <Main/>
-                <Main/>
+            <div >
+            <reducer/>
+            <task/>
             </div>
         );
     }
 }
 export default App;
+
+
